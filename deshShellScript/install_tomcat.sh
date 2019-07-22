31c03601fdb7ff3ad2342e370202b8891a73a7f4
root <root@ip-172-31-23-20.ap-south-1.compute.internal>
2019-07-22 07:15:03 +0000

    Modify the script

--- deshShellScript/install_tomcat.sh
+++ deshShellScript/install_tomcat.sh
@@ -10,5 +10,12 @@ rpm -qa | grep -i java|tail -2|head -1 >/dev/null
 if [ $? -eq 0 ] 
 then
 echo -e "Java is already installed \n"
-
 fi
+
+echo -e "geting tomcat webserver \n"
+yum update -y >/dev/null
+yum install wget -y >/dev/null
+cd /opt
+wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.43/bin/apache-tomcat-8.5.43.tar.gz 
+tar -xvzf apache-tomcat-8.5.43.tar.gz >/dev/null
+rm -rf apache-tomcat-8.5.43.tar.gz >/dev/null
