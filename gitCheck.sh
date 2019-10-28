d6d1e84926d11b333bf8d91b7ecaaa0d4b4af815
Deshraj Sharma <deshrajsharma@gmail.com>
2019-10-28 08:14:45 +0000

    adding

--- gitCheck.sh
+++ gitCheck.sh
@@ -8,14 +8,24 @@ then
 fi
 
 echo "Git is installed "
+echo "setting user.name and user.email"
 git config --global user.name "Deshraj Sharma"
 git  config --global user.email "deshrajsharma@gmail.com"
 git remote -v
+
+echo "Checking out to master branch"
 git checkout master
+
+echo "Execuitng pull"
 git pull --rebase
+
 echo "My name is Deshraj" >> abc
+echo "adding abc in stage"
 git add abc
+echo "commit it to local repo"
 git commit -m "Adding abc file"
+
+echo "finlly to git repo"
 git push 
 
 
