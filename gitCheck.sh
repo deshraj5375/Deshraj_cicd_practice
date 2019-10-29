5d7f8ea3cee975e472aa35f3f0d01abd2d76156f
Deshraj Sharma <deshrajsharma@gmail.com>
2019-10-29 05:54:47 +0000

    Added

--- gitCheck.sh
+++ gitCheck.sh
@@ -7,27 +7,24 @@ then
 `sudo yum install git -y`
 fi
 
-echo "Git is installed "
-echo "setting user.name and user.email"
+echo "*********************Git is installed******************** "
 whoami
 
-git config --global user.name "Deshraj Sharma"
-git  config --global user.email "deshrajsharma@gmail.com"
 git remote -v
 
-echo "Checking out to master branch"
+echo "*****************Checking out to master branch******************"
 git checkout master
 
-echo "Execuitng pull"
+echo "********************Execuitng pull*******************"
 git pull --rebase
 
 echo "My name is Deshraj" >> abc
-echo "adding abc in stage"
+echo "****************************adding abc in stage****************************"
 git add abc
-echo "commit it to local repo"
+echo "**********************commit it to local repo*************************"
 git commit -m "Adding abc file"
 
-echo "finlly to git repo"
+echo "*****************************finlly to git repo**************************"
 git push 
 
 
